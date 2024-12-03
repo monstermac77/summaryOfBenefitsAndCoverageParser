4fd66cf499a9951a09c6af7aa52dbd79f72299f7
Joe Puccio <josephpuccio@gmail.com>
2024-12-03 10:03:39 -0500

    got it basically done

--- parseNYStateSHOPPlans.py
+++ parseNYStateSHOPPlans.py
@@ -6,12 +6,16 @@ import pprint
 
 # $(".comparePlan-tabs").click() to open everything
 
+# TODO: could improve the gathering of the HTML for sure, probably automate it, but the javascript loading of the page makes it hard
+
 html = open("plans/plan.html").read()
 soup = BeautifulSoup(html, "html.parser")
 
 def getCarrier(html):
 	if "emblemhealth.com" in html:
 		return "Emblem Health"
+	elif "UnitedHealthcare.png" in html:
+		return "United Healthcare"
 	return "Could not determine carrier"
 
 
@@ -81,19 +85,48 @@ for key, value in fields.items():
 		costName = key.replace("Raw", "")
 		# if it's a numberical value, then it's very straightforward 
 		if all([word not in value for word in ["Copay", "deductible"]]):
-			finalFields[costName+"BeforeDeductible"] = finalFields[costName+"AfterDeductible"] = value
+			finalFields[costName+"BeforeDeductible"] = finalFields[costName+"AfterDeductible"] = value.strip()
 		else:
 			# there's some sort of difference
 			if "Copay after deductible" in value:
 				raw = value.replace("Copay after deductible", "")
 				finalFields[costName+"BeforeDeductible"] = "FULL CHARGE"
-				finalFields[costName+"AfterDeductible"] = raw
+				finalFields[costName+"AfterDeductible"] = raw.strip()
+			if "Coinsurance after deductible" in value:
+				raw = value.replace("Coinsurance after deductible" , "")
+				processed = float(raw.strip().replace("%", "")) / 100
+				finalFields[costName+"BeforeDeductible"] = "FULL CHARGE"
+				finalFields[costName+"AfterDeductible"] = "PARTIAL CHARGE: {}".format(processed)
 
 pprint.pprint(finalFields)
 
-finalString = ""
-for column in ["carrier", "plan", "link", "level", "premium", "deductible", "outOfPocketMax", "therapyCostBeforeDeductible", "therapyCostAfterDeductible", "specialistCostBeforeDeductible", "specialistCostAfterDeductible", "primaryCareCostBeforeDeductible", "primaryCareCostAfterDeductible", "blooddrawCostBeforeDeductible", "blooddrawCostAfterDeductible", "psychiatristCostBeforeDeductible", "psychiatristCostAfterDeductible", "urgentCareCostBeforeDeductible", "urgentCareCostAfterDeductible", "surgeryCostBeforeDeductible", "surgeryCostAfterDeductible"]:
-	finalString += finalFields[column] + ", "
+# for when there's a full charge
+fieldsToColumnsMap = {
+	"therapyCost" : "C2",
+	"specialistCost" : "C3",
+	"primaryCareCost" : "C4",
+	"bloodDrawCost" : "C5",
+	"psychiatristCost" : "C6",
+	"urgentCareCost" : "C7",
+	"surgeryCost" : "C8"
+}
+
+finalString = '"SHOP NYS Marketplace", '
+for column in ["carrier", "plan", "link", "level", "premium", "deductible", "outOfPocketMax", "therapyCostBeforeDeductible", "therapyCostAfterDeductible", "specialistCostBeforeDeductible", "specialistCostAfterDeductible", "primaryCareCostBeforeDeductible", "primaryCareCostAfterDeductible", "bloodDrawCostBeforeDeductible", "bloodDrawCostAfterDeductible", "psychiatristCostBeforeDeductible", "psychiatristCostAfterDeductible", "urgentCareCostBeforeDeductible", "urgentCareCostAfterDeductible", "surgeryCostBeforeDeductible", "surgeryCostAfterDeductible"]:
+	value = finalFields[column]
+	chosenPair = None
+	for key, spreadsheetPair in fieldsToColumnsMap.items():
+		if column.startswith(key):
+			chosenPair = spreadsheetPair
+			break
+	if value == "FULL CHARGE": 
+		finalString += "=" + chosenPair + ", "
+	elif "PARTIAL CHARGE: " in value:
+		coinsurance = value.replace("PARTIAL CHARGE: ", "")
+		finalString += "={}*{}".format(chosenPair, coinsurance) + ", "
+	else:
+		finalString += '"{}", '.format(value)
+finalString = finalString.rstrip(", ")
 
 
 print(finalString)
