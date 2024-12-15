85ce2d13eab1039a0706996fca162bbab70154a5
Joe Puccio <josephpuccio@gmail.com>
2024-12-15 00:24:21 -0500

    got a couple more, but it's getting messy

--- parseSummaryOfBenefitsAndCoveragePDFs.py
+++ parseSummaryOfBenefitsAndCoveragePDFs.py
@@ -98,38 +98,63 @@ def extractSBCData(path):
 		reduced = reduced.split("/")[0]
 	outOfPocketMax = getNumberFromString(reduced)
 
+	# note: need to handle deductible does not apply for these 
+	
+
 	# therapy 
-	section = rawStripped.split("If you need mental health, behavioral health, or substance abuse services")[1].split("Office & other outpatient")[0].split("other outpatient services")[0]
-	therapyCostRaw = getNumberFromString(section.replace("Outpatient services Office:", "").strip())
+	section = rawStripped.split("If you need mental health, behavioral health, or substance abuse services")[1].replace(" Outpatient services Office: ", "").strip()
+	if " " in section:
+		section = section.split(" ")[0]
+	if section == "Outpatient":
+		section = rawStripped.split("If you need mental health, behavioral health, or substance abuse services")[1].split("Outpatient services Office & other outpatient services:")[1].strip()
+		if " " in section:
+			section = section.split(" ")[0]
+	therapyCostRaw = section.replace("Outpatient services Office:", "").strip()
+	if "$" not in therapyCostRaw and "%" not in therapyCostRaw:
+		therapyCostRaw = "Unknown"
 
 	# specialist 
 	section = rawStripped.split("Specialist visit ")[1].split("Office & other outpatient")[0].split("copay/visit")[0].split("copay per visit")[0]
+	if " " in section:
+		section = section.split(" ")[0]
 	specialistCostRaw = getNumberFromString(section)
 
 	# primary care
-	section = rawStripped.split("Primary care visit to treat an injury or illness")[1].split("Virtual Primary Care telemedicine provider")[0].split("copay/visit")[0]
+	section = rawStripped.split("Primary care visit to treat an injury or illness")[1].split("Virtual Primary Care telemedicine provider")[0].split("copay/visit")[0].strip()
+	if " " in section:
+		section = section.split(" ")[0]
 	primaryCareCostRaw = getNumberFromString(section)
 
 	# blood draw
-	section = rawStripped.split("Diagnostic test (x-ray, blood work)")[1].split("Imaging ")[0]
+	section = rawStripped.split("Diagnostic test (x-ray, blood work)")[1].split("Imaging ")[0].strip()
+	if "No charge for laboratory" in section:
+		section = "$0"
+	elif " " in section:
+		section = section.split(" ")[0]
 	bloodDrawRaw = section.split(" coinsurance ")[0]
 
 	# psychiatrist
 	psychiatristCostRaw = therapyCostRaw
 
 	# urgent care
-	section = rawStripped.split("Urgent care")[1].split("copay/visit")[0].split("copay per visit")[0]
+	section = rawStripped.split("Urgent care")[1].split("copay/visit")[0].split("copay per visit")[0].strip()
+	if " " in section:
+		section = section.split(" ")[0]
 	urgentCareRaw = getNumberFromString(section)
 
 	# surgery facilities
-	section = rawStripped.split("Facility fee (e.g., ambulatory surgery center)")[1].split("copay/visit")[0].split(" coinsurance")[0]
+	section = rawStripped.split("Facility fee (e.g., ambulatory surgery center)")[1].split("copay/visit")[0].split(" coinsurance")[0].strip()
+	if " " in section:
+		section = section.split(" ")[0]
 	if "%" in section:
 		surgeryFacilityRaw = getNumberFromString(section) + "%"
 	else:
 		surgeryFacilityRaw = getNumberFromString(section)
 
 	# surgery services
-	section = rawStripped.split("Physician/surgeon fees")[1].split("copay/visit")[0].split(" coinsurance")[0]
+	section = rawStripped.split("Physician/surgeon fees")[1].split("copay/visit")[0].split(" coinsurance")[0].strip()
+	if " " in section:
+		section = section.split(" ")[0]
 	if "%" in section:
 		surgeryServicesRaw = getNumberFromString(section) + "%"
 	else:
@@ -163,6 +188,7 @@ def extractSBCData(path):
 for file in glob.glob(f"summaryOfBenefitsAndCoveragePDFs/*.pdf"):
 	# print("Parsing {}...".format(file.split("/")[1]))
 	plan = extractSBCData(file)
+	pprint.pprint(plan)
 	#pprint.pprint(plan)
 	#continue
 	plan = cleanPlan(plan, "sbcPDF")
--- shared.py
+++ shared.py
@@ -46,12 +46,27 @@ def processPlan(plan, dataSource):
 			processedPlan[key] = value
 		else:
 			costName = key.replace("Raw", "")
+
+			# there are a number of plans where the copays and coinsurences listed are implicitly after the deductible
+			# these PDFs have "All copayment and coinsurance costs shown in this chart are after your deductible has been met, if a deductible applies."
+			# in them, so we could detect this probably, but doing so manually now
+			# TODO: would want this to be more general
+			if dataSource == "sbcPDF":
+				# so far all of these assume that the copays and coinsurences are after the deductible
+				if "%" in value:
+					value = value + " Coinsurance after deductible"
+				else: # "numberical" (lol)
+					value = value + " Copay after deductible"
+
 			# if it's a numberical value, then it's very straightforward 
 			if is_numerical(value):
 				processedPlan[costName+"BeforeDeductible"] = processedPlan[costName+"AfterDeductible"] = value.strip()
 			else:
 				# there's some sort of difference or it's weird formatting
-				if "Copay after deductible" in value:
+				if "no charge for laboratory" in value.lower():
+					processedPlan[costName+"BeforeDeductible"] = "0"
+					processedPlan[costName+"AfterDeductible"] = "0"
+				elif "Copay after deductible" in value:
 					raw = value.replace("Copay after deductible", "")
 					processedPlan[costName+"BeforeDeductible"] = "FULL CHARGE"
 					processedPlan[costName+"AfterDeductible"] = raw.strip()
