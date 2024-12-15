69cf79e4342ce7938e039eef667f408ef91d6629
Joe Puccio <josephpuccio@gmail.com>
2024-12-14 22:28:36 -0500

    all pdfs

--- misc.js
+++ misc.js
@@ -1,8 +1,20 @@
 // helping gather links from a page to scrape (pdfs)
-var links = "";
+// var links = "";
+var links = [];
 $(".download-link").each(function(index, element) {
-	links += $(this).find("a").attr("href") + ".pdf, ";
+	// links += $(this).find("a").attr("href") + ".pdf, ";
+	links.push($(this).find("a").attr("href") + ".pdf");
 });
-links = links.slice(0, -2);
+// links = links.slice(0, -2);
 console.log(links);
-// 
+
+// Function to open and download a single file in a new tab
+const openAndDownloadPDF = (url) => {
+	// Open the URL in a new tab
+	window.open(url, '_blank');
+};
+
+// Iterate over the list and open each file in a new tab
+links.forEach(link => {
+	openAndDownloadPDF(link);
+});
