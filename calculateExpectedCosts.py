5b1afb235855dfa49abc5f4e374727eefd6ae1cc
Joe Puccio <josephpuccio@gmail.com>
2024-12-16 13:50:04 -0500

    distribution should bec onsidered

--- calculateExpectedCosts.py
+++ calculateExpectedCosts.py
@@ -6,16 +6,21 @@ import sys
 import argparse
 
 # generate all the arguments
-# python3 calculateExpectedCosts.py --therapyVisits 26 --specialistVisits 6 --primaryCareVisits 3 --bloodDrawVisits 2 --psychiatristVisits 4 --urgentCareVisits 2 --surgeries 1 --prescriptionFills 12
+# python3 calculateExpectedCosts.py --therapyVisits 26 event --specialistVisits 6 random --primaryCareVisits 3 even --bloodDrawVisits 2 even --psychiatristVisits 4 even --urgentCareVisits 2 even --surgeries 1 random --prescriptionFills 12 even
 parser = argparse.ArgumentParser()
-parser.add_argument("--therapyVisits", help="the number of therapy sessions you expect to have in a given year", metavar='therapies')
-parser.add_argument("--specialistVisits", help="the number of specialist visits you expect to have in a given year", metavar='specialists')
-parser.add_argument("--primaryCareVisits", help="the number of primary care visits you expect to have in a given year", metavar='primaries')
-parser.add_argument("--bloodDrawVisits", help="the number of blood draws you expect to have in a given year", metavar='bloods')
-parser.add_argument("--psychiatristVisits", help="the number of psychiatrist visits you expect to have in a given year", metavar='psychiatrists')
-parser.add_argument("--urgentCareVisits", help="the number of urgent care visits you expect to have in a given year", metavar='urgentCares')
-parser.add_argument("--surgeries", help="the number of surgeries you expect to have in a given year", metavar='surgeries')
-parser.add_argument("--prescriptionFills", help="the number of prescription fills you expect to have in a given year", metavar='prescriptions')
+parser.add_argument("--therapyVisits", help="the number of therapy sessions you expect to have in a given year", nargs=2, metavar=('therapies','therapiesDistro'))
+parser.add_argument("--specialistVisits", help="the number of specialist visits you expect to have in a given year", nargs=2, metavar=('specialists','specialistsDistro'))
+parser.add_argument("--primaryCareVisits", help="the number of primary care visits you expect to have in a given year", nargs=2, metavar=('primaries','primariesDistro'))
+parser.add_argument("--bloodDrawVisits", help="the number of blood draws you expect to have in a given year", nargs=2, metavar=('bloodDraws','bloodDrawsDistro'))
+parser.add_argument("--psychiatristVisits", help="the number of psychiatrist visits you expect to have in a given year", nargs=2, metavar=('psychiatrists','psychiatristsDistro'))
+parser.add_argument("--urgentCareVisits", help="the number of urgent care visits you expect to have in a given year", nargs=2, metavar=('urgentCares','urgentCaresDistro'))
+parser.add_argument("--surgeries", help="the number of surgeries you expect to have in a given year", nargs=2, metavar=('surgeries','surgeriesDistro'))
+parser.add_argument("--prescriptionFills", help="the number of prescription fills you expect to have in a given year", nargs=2, metavar=('prescriptions','prescriptionsDistro'))
+
+# should output: 
+# best sequence with cost
+# worst sequence with cost
+# average cost
 
 # if the developer didn't specify what to run, give them some help
 if len(sys.argv) == 1:
@@ -36,24 +41,24 @@ columnMap = {
 	5 : "premium", 
 	6 : "deductible", 
 	7 : "outOfPocketMax", 
-	8 : "therapyCostBeforeDeductible",
-	9 : "therapyCostAfterDeductible",
-	10 : "specialistCostBeforeDeductible",
-	11 : "specialistCostAfterDeductible",
-	12 : "primaryCareCostBeforeDeductible",
-	13 : "primaryCareCostAfterDeductible",
-	14 : "bloodDrawCostBeforeDeductible",
-	15 : "bloodDrawCostAfterDeductible",
-	16 : "psychiatristCostBeforeDeductible",
-	17 : "psychiatristCostAfterDeductible",
-	18 : "urgentCareCostBeforeDeductible",
-	19 : "urgentCareCostAfterDeductible",
+	8 : "therapiesCostBeforeDeductible",
+	9 : "therapiesCostAfterDeductible",
+	10 : "specialistsCostBeforeDeductible",
+	11 : "specialistsCostAfterDeductible",
+	12 : "primariesCostBeforeDeductible",
+	13 : "primariesCostAfterDeductible",
+	14 : "bloodsDrawsCostBeforeDeductible",
+	15 : "bloodsDrawsCostAfterDeductible",
+	16 : "psychiatristsCostBeforeDeductible",
+	17 : "psychiatristsCostAfterDeductible",
+	18 : "urgentCaresCostBeforeDeductible",
+	19 : "urgentCaresCostAfterDeductible",
 	20 : "surgeryFacilityCostBeforeDeductible",
 	21 : "surgeryFacilityCostAfterDeductible",
 	22 : "surgeryServicesCostBeforeDeductible",
 	23 : "surgeryServicesCostAfterDeductible",
-	24 : "genericDrugCostBeforeDeductible",
-	25 : "genericDrugCostAfterDeductible"
+	24 : "prescriptionsCostBeforeDeductible",
+	25 : "prescriptionsCostAfterDeductible"
 }
 
 # we could ingest the processed data, which we probably want to do eventually, but a CSV intermediary is nice so that 
